3942071a3139744d865a5800a4fa8596fb2cdf87
Al Zaplin <zaplin.dev@gmail.com>
2025-02-23 22:30:05 +0300

    feat(PricingCard component)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
+<!-- ? A challenge to style with the TailwindCSS, so it looks close to this one: https://flowbite.com/blocks/marketing/pricing/ -->
 <script setup>
 import { useDark } from '@vueuse/core'
-import AppProse from './components/AppProse.vue'
+import PricingCard from './components/PricingCard.vue'
 
 const isDark = useDark()
 </script>
@@ -8,9 +9,54 @@ const isDark = useDark()
   <label class="absolute top-5 right-5 cursor-pointer">
     <input v-model="isDark" type="checkbox" class="" /> Dark mode
   </label>
-  <div class="m-10">
-    <AppProse />
+  <h1 class="mt-15 mb-5 text-center text-4xl font-bold">Designed for business teams like yours</h1>
+  <h2 class="mb-10 mx-auto text-center w-3xl text-2xl font-light text-gray-700 dark:text-gray-400">
+    Here we focus on markets where technology, innovation, and capital can unlock long-term value
+    and drive economic growth.
+  </h2>
+  <div class="p-5 flex flex-wrap justify-center gap-8">
+    <PricingCard
+      :price="29"
+      :features="[
+        'Unlimited Access to All Features',
+        '24/7 Customer Support',
+        'Customizable Dashboard',
+        'Advanced Analytics Tools',
+        'Integration with Third-Party Services',
+        'Secure Data Encryption',
+      ]"
+      name="Starter"
+      description="Best option for personal use & for your next project"
+    ></PricingCard>
+    <PricingCard
+      name="Plus"
+      :features="[
+        'Multi-User Collaboration Tools',
+        'Real-Time Data Backup',
+        'Mobile App Access',
+        'Custom Branding Options',
+        'Automated Reporting System',
+      ]"
+      :price="50"
+      description="Great for small teams and anyone looking to scale"
+    ></PricingCard>
+    <PricingCard
+      name="Enterprise"
+      :features="[
+        'Priority Feature Updates',
+        'Extended Data Storage Capacity',
+        'Personalized User Experience',
+        'In-Depth Training Materials',
+        'Dedicated Account Manager',
+      ]"
+      :price="150"
+      description="Best for large scale uses and extended redistribution rights"
+    ></PricingCard>
   </div>
 </template>
 
-<style></style>
+<style>
+body {
+  font-family: Inter;
+}
+</style>
---
+++ src/components/PricingCard.vue
@@ -0,0 +1,48 @@
+<script setup>
+defineProps({
+  price: { type: Number, default: 0 },
+  features: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+})
+</script>
+<template>
+  <div
+    class="flex flex-col items-center basis-96 p-9 text-center border-gray-600 border-1 rounded-lg dark:bg-gray-800"
+  >
+    <div>
+      <h3 class="mb-4 text-3xl font-medium">{{ name }}</h3>
+      <p class="mb-9 text-lg font-light text-gray-700 dark:text-gray-400">
+        {{ description }}
+      </p>
+      <div class="mb-9">
+        <span class="text-5xl font-bold">${{ price }}</span>
+        <span class="ml-3 text-gray-700 dark:text-gray-400">/month</span>
+      </div>
+      <!-- List -->
+      <ul class="pb-5" role="list">
+        <li class="mb-5 text-left" v-for="feature in features" :key="feature">
+          <!-- Icon -->
+          <svg
+            class="mr-3 inline-block w-5 h-5 text-green-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="text-md font-medium">{{ feature }}</span>
+        </li>
+      </ul>
+    </div>
+    <a
+      class="block mt-auto w-full p-3.5 bg-teal-600 rounded-lg hover:bg-teal-500 active:bg-teal-700 transition-colors"
+      href="#"
+      >Get started</a
+    >
+  </div>
+</template>
